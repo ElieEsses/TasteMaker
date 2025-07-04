bbe7096ce6b782c7fa337e5dbce76b2558463756
Elie Esses <elieesses04@gmail.com>
2025-07-03 22:05:36 -0400

    spotify api class

---
+++ Project/SpotifyAPI.py
@@ -0,0 +1,4 @@
+class SpotifyAPI:
+    def __init__(self, clinet_id, client_secret):
+        self.client_id = clinet_id
+        self.client_secret = client_secret
--- Project/main.py
+++ Project/main.py
@@ -1,2 +1,11 @@
+import os
 from dotenv import load_dotenv
 load_dotenv()
+
+from SpotifyAPI import SpotifyAPI
+
+spotify_client_id = os.getenv("SPOTIFY_CLIENT_ID")
+spotify_client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
+
+spotify_client = SpotifyAPI(spotify_client_id, spotify_client_secret)
+ 
